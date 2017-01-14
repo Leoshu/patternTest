336407e7794d53f73f87d0d1aa3545851e737f54
Leo_hsu <leo_hsu@gemteks.com>
2017-01-14 21:28:06 +0800

    Initial Commit

---
+++ PatenTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PatenTest
+//
+//  Created by Leo_hsu on 2017/1/14.
+//  Copyright © 2017年 Leo_hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PatenTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PatenTest
+//
+//  Created by Leo_hsu on 2017/1/14.
+//  Copyright © 2017年 Leo_hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
